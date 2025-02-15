0dd94f985e8a9c70155c1e497bb8c28af857ef39
mddaf <md.daf001@gmail.com>
2025-02-15 21:45:25 +0600

    setup 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import Header from "./components/Header"
+import Banner from "./components/Banner"
+// import Players from "./components/Players"
 
-function App() {
-
+const App =() =>{
   return (
-    <>
-      <h1 className='text-6xl bg-red-300'>bpl-dream-xi</h1>
-    </>
-  )
+  <div>
+    <Header></Header>
+    <Banner></Banner>
+  </div>
+)
 }
 
 export default App
---
+++ src/components/Players.jsx
@@ -0,0 +1,17 @@
+// import { useEffect } from "react";
+// import { useState } from "react"
+
+// const Players = () =>{
+//     const [players, setPlayers] = useState([]);
+
+//     useEffect(()=>{
+//         fetch('players.json')
+//         .then(res => res.json())
+//         .then(data=>setPlayers(data))
+//     },[])
+//     return(
+//         <div></div>
+//     );
+// };
+
+// export default Players;
